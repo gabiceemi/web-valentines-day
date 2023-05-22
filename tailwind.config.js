63d5f336d0026dd5e708d0f1193fc2c5a019583b
Gabriel Cordova Medeiros <gabiceemi@gmail.com>
2023-05-22 17:33:13 -0300

    starting application

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,66 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: 'var(--font-roboto)',
+        alt: 'var(--font-bai-jamjuree)',
+      },
+      colors: {
+        gray: {
+          50: '#eaeaea',
+          100: '#bebebf',
+          200: '#9e9ea0',
+          300: '#727275',
+          400: '#56565a',
+          500: '#2c2c31',
+          600: '#28282d',
+          700: '#1f1f23',
+          800: '#18181b',
+          900: '#121215',
+        },
+        red: {
+          50: '#faebeb',
+          100: '#f8e0e0',
+          200: '#f0bfbf',
+          300: '#cf3232',
+          400: '#ba2d2d',
+          500: '#a62828',
+          600: '#9b2626',
+          700: '#7c1e1e',
+          800: '#5d1616',
+          900: '#481212',
+        },
+        green: {
+          50: '#e6fbef',
+          100: '#b1f1ce',
+          200: '#8cebb6',
+          300: '#57e295',
+          400: '#36dc81',
+          500: '#04d361',
+          600: '#04c058',
+          700: '#039645',
+          800: '#027435',
+          900: '#025929',
+        },
+      },
+
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        stripes:
+          'linear-gradient(to bottom, rgba(207, 50, 50), rgba(207, 50, 50) 12.5%, transparent 12.5%, transparent)',
+      },
+
+      fontSize: {
+        '5xl': '2.5rem',
+      },
+
+      backgroundSize: {
+        stripes: '100% 8px',
+      },
+
+      blur: {
+        full: '194px',
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
